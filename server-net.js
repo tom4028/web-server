0bbd24cf74f83267b8f102e9e523a621e2a70e08
Tom Jack <alba4028@o2.pl>
2016-09-18 21:07:29 +0200

    middleware

--- server-net.js
+++ server-net.js
@@ -1,21 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
- var contents = fs.readFile('package.json',function(err,buf){
-	 return buf.toString();
- });
+ 
 
 // var contents = fs.readFileSync('package.json').toString();
 // console.log(contents);
 
 var server = http.createServer(function(request,response){
 	response.writeHead(200,{"Content-type":"text/plain"});
+	
+	var contents = fs.readFile('package.json',function(err,buf){
+		buf.toString();
+	});
 	response.end(contents);
 });
-
 server.listen(7000);
-
-
-
-
-
